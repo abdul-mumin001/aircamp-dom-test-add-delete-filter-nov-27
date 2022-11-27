7d320efd6d807be6828ec38ea23b01b4245ac2b5
abdul-mumin001 <abdulmuminbar@gmail.com>
2022-11-27 18:24:10 +0530

    dom-test

---
+++ dom-test/app.js
@@ -0,0 +1,36 @@
+
+const products=document.querySelector("products");
+const container=document.querySelector(".container");
+
+
+document.querySelector("form").addEventListener("submit", appendProduct);
+container.addEventListener("click", deleteDiv);
+
+function appendProduct(event){
+    event.preventDefault();
+    if(document.querySelector("#url").value.length==0)
+    alert("enter url");
+    else{
+    let div=document.createElement("div");
+    div.setAttribute("class","item")
+    const url=document.querySelector("#url").value;
+    let image=document.createElement("img");
+    image.src=url;
+
+    let btn=document.createElement("button");
+    btn.innerText="Delete";
+    btn.setAttribute("class","delete_btn")
+
+    div.append(image, btn);
+    container.append(div);
+    document.querySelector("#url").value="";
+}
+}
+function deleteDiv(e){
+    console.log(e.target);
+    if(!(e.target.classList.contains("delete_btn")))
+    return
+    const delBtn=e.target;
+    delBtn.parentElement.remove();
+
+}
